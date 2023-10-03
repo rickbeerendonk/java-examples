005301e05782284abf645b0c4a2b0e74812322a7
Rick Beerendonk <rick@beerendonk.com>
2023-10-03 13:03:24 +0200

    Rename.

--- 01 - Simple/a. Console/Console.java
+++ 01 - Simple/a. Console/Console.java
@@ -1,7 +1,7 @@
 // European Union Public License version 1.2
 // Copyright © 2023 Rick Beerendonk
 
-public class Simple {
+public class Console {
   public static void main(String... args) {
   	System.out.println("Hello Java!");
   }
