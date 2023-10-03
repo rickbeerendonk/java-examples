8ff7e107d0bedd5f43da48df4e50b5209582a44c
Rick Beerendonk <rick@beerendonk.com>
2023-10-03 22:19:55 +0200

    Add "Comments" example.

---
+++ 02 - Comments/Comments.java
@@ -0,0 +1,25 @@
+// European Union Public License version 1.2
+// Copyright © 2023 Rick Beerendonk
+
+public class Comments {
+  public static void main(String... args) {
+
+    // One line
+
+    System.out.println("1");
+
+    /*
+    Multi
+    line
+    */
+
+    System.out.println("2");
+
+    /*
+    Multi
+      // Nested line
+    */
+
+    System.out.println("3");
+  }
+}
