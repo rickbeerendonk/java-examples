e725c0abe81bddcec8b92de14de33d15284b47e2
Rick Beerendonk <rick@beerendonk.com>
2023-10-11 22:01:26 +0200

    Add "Variables - Var" example.

---
+++ 03 - Variables/a. Var/Var.java
@@ -0,0 +1,22 @@
+// European Union Public License version 1.2
+// Copyright © 2023 Rick Beerendonk
+
+public class Comments {
+  public static void main(String... args) {
+    // Explicit type
+    int expl = 123;
+    
+    // Implicit type
+    var impl = 456; // int, not an object
+    
+    Integer explObj = expl;
+    Integer implObj = impl;
+    System.out.println(explObj.getClass()); // class java.lang.Integer
+    System.out.println(implObj.getClass()); // class java.lang.Integer
+    
+    // No error (as opposed to const)
+    expl = impl;
+    
+    System.out.println(expl); // 456
+  }
+}
