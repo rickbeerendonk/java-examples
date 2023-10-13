90710d9ed03e19c0a4b1a33cf8ffce8c7b547862
Rick Beerendonk <rick@beerendonk.com>
2023-10-13 22:08:35 +0200

    Add "Types - Boolean" example.

---
+++ 04 - Types/a. Boolean/Boolean.java
@@ -0,0 +1,14 @@
+// European Union Public License version 1.2
+// Copyright © 2023 Rick Beerendonk
+
+public class Boolean {
+  public static void main(String... args) {
+        System.out.println(true);
+        System.out.println(false);
+        
+        System.out.println(Boolean.class.getName());               // Boolean 
+        System.out.println(Boolean.class.getSimpleName());         // Boolean
+        System.out.println(Boolean.class.getCanonicalName());      // Boolean
+        System.out.println(Boolean.class.getTypeName());           // Boolean
+  }
+}
