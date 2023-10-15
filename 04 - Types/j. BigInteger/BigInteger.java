23a437e79be9ced64426958a0b5b476902f8957c
Rick Beerendonk <rick@beerendonk.com>
2023-10-15 17:26:46 +0200

    Add "Types - BigInteger" example.

---
+++ 04 - Types/j. BigInteger/BigInteger.java
@@ -0,0 +1,15 @@
+// European Union Public License version 1.2
+// Copyright © 2023 Rick Beerendonk
+
+import java.math.BigInteger;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(BigInteger.ONE); 
+        System.out.println(BigInteger.TEN); 
+        System.out.println(BigInteger.ZERO);
+
+        BigInteger bi = new BigInteger("123");
+        System.out.println(bi);
+    }
+}
