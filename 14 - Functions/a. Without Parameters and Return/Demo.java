482f33cfc8d70bf44225a3456bcf4ad90fe6dac3
Rick Beerendonk <rick@beerendonk.com>
2023-10-15 17:37:10 +0200

    Add "Functions - Without Parameters and Return" example.

---
+++ 14 - Functions/a. Without Parameters and Return/Demo.java
@@ -0,0 +1,14 @@
+// European Union Public License version 1.2
+// Copyright © 2023 Rick Beerendonk
+
+package org.functions.withoutparametersreturn;
+
+public class Demo {
+    public static void one() {
+        System.out.println("One");
+    }
+
+    public static void main(String[] args) {
+        one();
+    }
+}
