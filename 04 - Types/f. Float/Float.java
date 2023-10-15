5fe8a415b92224a42cc1a6e8f560bde991ac2121
Rick Beerendonk <rick@beerendonk.com>
2023-10-15 15:09:26 +0200

    Align comments.

--- 04 - Types/f. Float/Float.java
+++ 04 - Types/f. Float/Float.java
@@ -5,15 +5,15 @@ public class Main {
     public static void main(String[] args) {
       // Min and Max
 
-        System.out.println("Min:          " + Float.MIN_VALUE); // 1.4E-45
-        System.out.println("Max:          " + Float.MAX_VALUE); // 3.4028235E38
-        System.out.println("Min Normal:   " + Float.MIN_NORMAL); // 1.1754944E-38
-        System.out.println("Min Exponent: " + Float.MIN_EXPONENT); // -126
-        System.out.println("Max Exponent: " + Float.MAX_EXPONENT); // 127
+        System.out.println("Min:          " + Float.MIN_VALUE);         // 1.4E-45
+        System.out.println("Max:          " + Float.MAX_VALUE);         // 3.4028235E38
+        System.out.println("Min Normal:   " + Float.MIN_NORMAL);        // 1.1754944E-38
+        System.out.println("Min Exponent: " + Float.MIN_EXPONENT);      // -126
+        System.out.println("Max Exponent: " + Float.MAX_EXPONENT);      // 127
         System.out.println("Neg Infinity: " + Float.NEGATIVE_INFINITY); // -Infinity
         System.out.println("Pos Infinity: " + Float.POSITIVE_INFINITY); // Infinity
-        System.out.println("Bytes:        " + Float.BYTES);     // 4
-        System.out.println("Size:         " + Float.SIZE);      // 32
+        System.out.println("Bytes:        " + Float.BYTES);             // 4
+        System.out.println("Size:         " + Float.SIZE);              // 32
 
         // Explicit vs Implicit
         
