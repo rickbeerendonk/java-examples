f18c57e1208a70729ee75599a84f5be045648f27
Rick Beerendonk <rick@beerendonk.com>
2023-10-15 11:30:52 +0200

    Add "Variables - Multiple - Different Value" example.

---
+++ 03 - Variables/c. Multiple/i. Different Value.java
@@ -0,0 +1,11 @@
+// European Union Public License version 1.2
+// Copyright © 2023 Rick Beerendonk
+
+public class MutlipleVars_DifferentValue {
+  public static void main(String[] args) {
+    int a = 1, b = 2, c = 3;
+    System.out.println(a); // 1
+    System.out.println(b); // 2
+    System.out.println(c); // 3
+  }
+}
