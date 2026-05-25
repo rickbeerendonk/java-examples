f4c71d6758a770d27e2927ca4060c6021865b43a
Rick Beerendonk <rick@beerendonk.com>
2026-05-25 10:42:02 +0200

    Add "Loops - While" example.

---
+++ 09 - Loops/a. while/While.java
@@ -0,0 +1,23 @@
+// European Union Public License version 1.2
+// Copyright © 2026 Rick Beerendonk
+
+package org.loops.while_;
+
+public class While {
+    public static void main(String[] args) {
+        int i = 1;
+
+        while (i < 5) {
+            System.out.println("Iteration " + i);
+            i++;
+        }
+
+        System.out.println("End");
+    }
+}
+
+// Iteration 1
+// Iteration 2
+// Iteration 3
+// Iteration 4
+// End
